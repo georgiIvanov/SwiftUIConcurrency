8d3583dee131ce9afb6a46e331d9b734da6c7c89
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2022-07-13 17:17:27 +0200

    Initial Commit

---
+++ SwiftUIConcurrency/SwiftUIConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConcurrencyApp.swift
+//  SwiftUIConcurrency
+//
+//  Created by Georgi Ivanov on 13.07.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
